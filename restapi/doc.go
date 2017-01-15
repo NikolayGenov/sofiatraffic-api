0944b14f18a64977a763c06eec13c048642c0855
Nikolay Genov <nikolay.genov2@gmail.com>
2017-01-15 23:07:12 +0200

    Upload autogenerated files, update README with some usage

---
+++ restapi/doc.go
@@ -0,0 +1,22 @@
+/*Package restapi Sofia Traffic API
+
+A better API for Sofia Traffic service
+
+
+    Schemes:
+      https
+    Host: localhost
+    BasePath: /v1
+    Version: 1.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
